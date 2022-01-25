4375e7cdad281d0af0cf10f9b2545d177fffceec
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-24 14:45:11 -1000

    Add user login with password hashing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,30 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  // expects {email: 'user@email.com', password:'secret123'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(404).json({ message: "No user with that email address." });
+      return;
+    }
+
+    // res.json({ user: dbUserData });
+
+    // verify User
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password" });
+      return;
+    }
+    res.json({ user: dbUserData, message: "You are now logged in" });
+  });
+});
+
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
   // expects {username: 'Username', email: 'name@email.com', password: 'Password'}
