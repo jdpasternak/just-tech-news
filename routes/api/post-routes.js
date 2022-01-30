2d79c844ba4373838a5353c2fc7ef2e9352eb32a
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-30 12:03:44 -1000

    Add Vote model, update realationship, add upvote route, update Post routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,23 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Vote } = require("../../models");
+const sequelize = require("../../config/connection");
 
 // get all Posts
 router.get("/", (req, res) => {
   console.log("========================");
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     order: [["created_at", "DESC"]],
     include: [{ model: User, attributes: ["username"] }],
   })
@@ -16,6 +28,37 @@ router.get("/", (req, res) => {
     });
 });
 
+// PUT /api/posts/upvote
+// cast a vote on a post
+router.put("/upvote", (req, res) => {
+  console.log("upvoting");
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+  })
+    .then(() => {
+      return Post.findOne({
+        where: {
+          id: req.body.post_id,
+        },
+        attributes: [
+          "id",
+          "post_url",
+          "title",
+          "created_at",
+          [
+            sequelize.literal(
+              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+            ),
+            "vote_count",
+          ],
+        ],
+      });
+    })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => res.json(err));
+});
+
 // GET a single Post
 router.get("/:id", (req, res) => {
   console.log("===========================");
@@ -23,7 +66,18 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       {
         model: User,
