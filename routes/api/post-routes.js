f1f389b3ed604b6a2318a2bf13a9acaa3b09d451
Jake Pasterank <jdp.pasternak@gmail.com>
2022-01-30 12:57:54 -1000

    Update post-routes to include Comment

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
 // get all Posts
@@ -19,7 +19,14 @@ router.get("/", (req, res) => {
       ],
     ],
     order: [["created_at", "DESC"]],
-    include: [{ model: User, attributes: ["username"] }],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: { model: User, attributes: ["username"] },
+      },
+      { model: User, attributes: ["username"] },
+    ],
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -59,6 +66,11 @@ router.get("/:id", (req, res) => {
       ],
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: { model: User, attributes: ["username"] },
+      },
       {
         model: User,
         attributes: ["username"],
